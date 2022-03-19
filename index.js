fd247f77e4dcc698c49e4990c7d05f0b0abcb864
KaonWry <titan2011official@gmail.com>
2022-03-19 10:56:52 +0700

    Fix

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 //NPM dependencies
 var prompt = require('prompt-sync')();
 //The rules
-console.log("This is a simple game.\nThe goal?\nReach a certain agreed number.\nHow?\nAll player starts at number one, each player add the number either 1, 2, or 3 to the number before.\nThe winner is the player who reach the agreed number.\nThere can be only one winner.");
 console.log("This is a simple game.\nThe goal?\nReach a certain agreed number.\nHow?\nAll player starts at number one, each player add the number either 1, 2, 3, or 4 to the number before.\nThe winner is the player who reach the agreed number.\nThere can be only one winner.");
 //Initialisation
 var mason = 1;
