ffd2220a292f2adff305295f3d7f9311418899f3
KaonWry <titan2011official@gmail.com>
2022-03-20 10:28:18 +0700

    Added the "Play again" function
    Added it so you dont have to open the file again and again to play it

--- index.js
+++ index.js
@@ -1,39 +1,58 @@
 //Dependencies
 var prompt = require('prompt-sync')();
 
-
 //The rules
 console.log("This is a simple game.\nThe goal?\nReach a certain agreed number.\nHow?\nAll player starts at number one, each player add the number either 1, 2, 3, or 4 to the number before.\nThe winner is the player who reach the agreed number.\nThere can be only one winner.");
-console.log("[1] Singleplayer (UNSTABLE)");
-console.log("[2] Multiplayer");
 
-
-//Gamemode
 while (true) {
-   var gamemode = prompt("Choose gamemode: ");
-   gamemode = parseInt(gamemode);
-   if (gamemode == 1) {
-      require('./singleplayer.js')();
-      break;
+   console.log("[1] Singleplayer (UNSTABLE)");
+   console.log("[2] Multiplayer");
+
+   //Gamemode
+   while (true) {
+      var gamemode = prompt("Choose gamemode: ");
+      gamemode = parseInt(gamemode);
+      if (gamemode == 1) {
+         require('./singleplayer.js')();
+         break;
+      }
+      else if (gamemode == 2) {
+         require('./multiplayer.js')();
+         break;
+      }
+      else {
+         continue;
+      }
    }
-   else if (gamemode == 2) {
-      require('./multiplayer.js')();
-      break;
+
+   //Initialation
+   var mason = 1;
+   var turn  = 1;
+   var playerTyped;
+   var maxMason = prompt("Type the highest number: ");
+   maxMason = parseInt(maxMason);
+   console.log(mason + "/" + maxMason);
+
+   //Game init
+   playNow(mason, maxMason, turn, playerTyped);
+
+   //Play again function
+   console.log("Want to play again?")
+   console.log("Type yes/y or no/n");
+   while (true) {
+      var playAgain = prompt();
+      playAgain = playAgain.toLowerCase();
+      if (playAgain == "yes" || playAgain == "y" || playAgain == "no" || playAgain == "n") {
+         break;
+      }
+      else {
+         continue;
+      }
    }
-   else {
+   if (playAgain == "yes" || playAgain == "y") {
       continue;
    }
+   else if (playAgain == "no" || playAgain == "n") {
+      break;
+   }
 }
-
-
-//Initialisation
-var mason = 1;
-var turn  = 1;
-var playerTyped;
-var maxMason = prompt("Type the highest number: ");
-maxMason = parseInt(maxMason);
-console.log(mason + "/" + maxMason);
-
-
-//Game init
-playNow(mason, maxMason, turn, playerTyped);
